49c67fc84a68cd66b449de3d392dff0892511d83
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-31 15:07:13 -0400

    Counter is able to count up and down

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -1,19 +1,58 @@
-import React from 'react';
-const Form = (props) =>{
-    let classList = " "
-    if(props.medium){
-        classList = " medium-form"
+import React, { Component } from 'react';
+
+
+// This is a functional component - just sent up a little differently as an arrow function!
+class  Form  extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            count: this.props.value,
+            max: this.props.max, 
+            min: this.props.min
+        }
+        this.increaseCount = this.increaseCount.bind(this);
+        this.handleClickPlus = this.handleClickPlus.bind(this); 
+        this.handleClickMinus = this.handleClickMinus.bind(this);
+
     }
-    if(props.large){
-        classList = " large-form";
+    increaseCount(currentValue){
+    return {count: currentValue.count + 1}
+    }
+
+    decreaseCount(currentValue){
+        return {count: currentValue.count - 1}
     }
     
+    handleClickPlus(){
+        if(this.state.count < this.state.max){
+            this.setState(this.increaseCount)
+        }
+       
+    }
+
+    handleClickMinus(){
+        if(this.state.count > this.state.min){
+            this.setState(this.decreaseCount)
+        }
+       
+    }
+
+    render(){
+        let classList = " "
+        if(this.props.counter){
+            classList += " counter"
+        }
+    
     return (
-    <label>
-        {props.label }
-        <br></br><input className = {classList} type ={props.type} placeholder = {props.placeholder} /> 
-    </label>
+    <form action="">
+        <label htmlFor="">{this.props.label}</label>
+        <input type={this.props.type} value={this.props.minus} className={classList} onClick = {this.handleClickMinus}/>
+        <span className={classList}>{this.state.count}</span>
+        {/* <input type={this.props.type} placeholder={this.props.placeholder} value={this.state.count} onClick={this.increaseCount}/> */}
+        <input type={this.props.type} value={this.props.plus} className={classList} onClick = {this.handleClickPlus}/>
+    </form>
     )
+}
 }
 
 export default Form;
--- src/components/Forms/Forms.stories.js
+++ src/components/Forms/Forms.stories.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import './Forms.css';
 import Form from './Forms';
+import arrowDown from './arrow down.png'
 
 storiesOf('Forms', module)
-.add('Small Text Input', () => <Form label ="Email" placeholder="Email" type="text"/>)
-.add('Medium Text Input', () => <Form label ="Email" placeholder="Email" type="text" medium/>)
-.add('Large Text Forms', () => <Form label ="Email" placeholder="Email" type="text" large />)
+.add('Submit Button', () => <Form type = "text" placeholder="Voucher Code" />)
+.add('Counter', () => <Form type = "button" value= {2} plus="+" minus="-" max={10} min={0}counter/>)
+.add('Select Button', () => <Form type = "text" label="Email" placeholder="Email"/>)
